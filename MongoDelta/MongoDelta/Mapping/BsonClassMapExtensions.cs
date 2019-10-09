5b8b56eb1e99cd4ab2d4f2d31db8c06d110c1329
Adrian Bradfield <adrian.bradfield@outlook.com>
2019-10-09 14:17:06 +0100

    #34 Added XML docs for public members

--- MongoDelta/MongoDelta/Mapping/BsonClassMapExtensions.cs
+++ MongoDelta/MongoDelta/Mapping/BsonClassMapExtensions.cs
@@ -6,12 +6,20 @@ using MongoDB.Bson.Serialization;
 
 namespace MongoDelta.Mapping
 {
+    /// <summary>
+    /// Contains extension methods to use whilst created a BsonClassMap
+    /// </summary>
     public static class BsonClassMapExtensions
     {
         private static readonly ReaderWriterLockSlim ConfigLock = new ReaderWriterLockSlim();
         private static readonly Dictionary<Type, DeltaUpdateConfiguration> UpdateConfigurations =
             new Dictionary<Type, DeltaUpdateConfiguration>();
 
+        /// <summary>
+        /// When an item of this type is updated, mapped members should only be updated if they have changed
+        /// </summary>
+        /// <param name="classMap">The class map for the item type</param>
+        /// <returns>The class map</returns>
         public static BsonClassMap UseDeltaUpdateStrategy(this BsonClassMap classMap)
         {
             ChangeConfigWithWriteLock(classMap, config => config.EnableDeltaUpdateStrategy());
@@ -23,6 +31,12 @@ namespace MongoDelta.Mapping
             return GetConfigValueWithReadLock(classMap, config => config.UseDeltaUpdateStrategy);
         }
 
+        /// <summary>
+        /// Specifies that the member should be updated incrementally. Only valid for members that serialize to the
+        /// BSON types: Int32, Int64, Double or Decimal128
+        /// </summary>
+        /// <param name="memberMap">The map for the member to update incrementally</param>
+        /// <returns>The member map</returns>
         public static BsonMemberMap UpdateIncrementally(this BsonMemberMap memberMap)
         {
             ChangeConfigWithWriteLock(memberMap.ClassMap,
--- MongoDelta/MongoDelta/Mapping/UpdateIncrementallyAttribute.cs
+++ MongoDelta/MongoDelta/Mapping/UpdateIncrementallyAttribute.cs
@@ -3,9 +3,14 @@ using MongoDB.Bson.Serialization;
 
 namespace MongoDelta.Mapping
 {
+    /// <summary>
+    /// Specifies that the member should be updated incrementally. Only valid for members that serialize to the
+    /// BSON types: Int32, Int64, Double or Decimal128
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class UpdateIncrementallyAttribute : Attribute, IBsonMemberMapAttribute
     {
+        /// <inheritdoc />
         public void Apply(BsonMemberMap memberMap)
         {
             memberMap.UpdateIncrementally();
