19706a322f0be9d6cc9a3b1e181cd6664710118d
Adrian Bradfield <adrian.bradfield@outlook.com>
2019-10-09 14:22:37 +0100

    #34 Added XML docs for public members (#36)

--- MongoDelta/MongoDelta/Mapping/UseDeltaUpdateStrategyAttribute.cs
+++ MongoDelta/MongoDelta/Mapping/UseDeltaUpdateStrategyAttribute.cs
@@ -3,9 +3,13 @@ using MongoDB.Bson.Serialization;
 
 namespace MongoDelta.Mapping
 {
+    /// <summary>
+    /// When an item of this type is updated, mapped members should only be updated if they have changed
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class UseDeltaUpdateStrategyAttribute : Attribute, IBsonClassMapAttribute
     {
+        /// <inheritdoc />
         public void Apply(BsonClassMap classMap)
         {
             classMap.UseDeltaUpdateStrategy();
--- MongoDelta/MongoDelta/UnitOfWorkBase.cs
+++ MongoDelta/MongoDelta/UnitOfWorkBase.cs
@@ -5,6 +5,9 @@ using MongoDB.Driver;
 
 namespace MongoDelta
 {
+    /// <summary>
+    /// An abstract base class for a UnitOfWork
+    /// </summary>
     public abstract class UnitOfWorkBase
     {
         private readonly IMongoDatabase _database;
@@ -12,22 +15,42 @@ namespace MongoDelta
         private readonly Dictionary<Type, MongoDeltaRepository> _repositories = new Dictionary<Type, MongoDeltaRepository>();
         private bool _hasCommitted;
 
+        /// <summary>
+        /// Constructor for the base class
+        /// </summary>
+        /// <param name="database">An instance of the MongoDatabase to connect to</param>
+        /// <param name="useTransactions">Set to true to use multi-document transactions</param>
         protected UnitOfWorkBase(IMongoDatabase database, bool useTransactions = true)
         {
             _database = database;
             _useTransactions = useTransactions;
         }
 
+        /// <summary>
+        /// Creates a repository and links it to a collection
+        /// </summary>
+        /// <param name="collectionName">The name of the collection to link to</param>
+        /// <typeparam name="TAggregate">The type of model to use for the collection</typeparam>
         protected void RegisterRepository<TAggregate>(string collectionName) where TAggregate : class
         {
             var repository = new MongoDeltaRepository<TAggregate>(_database.GetCollection<TAggregate>(collectionName));
             _repositories.Add(typeof(TAggregate), repository);
         }
 
+        /// <summary>
+        /// Gets a repository that has previously been registered
+        /// </summary>
+        /// <typeparam name="TAggregate">The type of model to get a repository for</typeparam>
+        /// <returns>The repository</returns>
         protected MongoDeltaRepository<TAggregate> GetRepository<TAggregate>() where TAggregate : class
         {
             return (MongoDeltaRepository<TAggregate>) _repositories[typeof(TAggregate)];
         }
+
+        /// <summary>
+        /// Commits changes on all repositories that have been registered
+        /// </summary>
+        /// <returns>void</returns>
         public async Task CommitAsync()
         {
             if (_hasCommitted)
